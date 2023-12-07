e2f444359cd3e2272d0a42694a84bb6c8a8826b7
Ian Flynn <ianflynnwork@gmail.com>
2023-12-07 18:14:12 -0500

    Add part one solution for Day 2

--- Day2.js
+++ Day2.js
@@ -5,6 +5,40 @@ let input = fs.readFileSync(
   path.join(__dirname, '/Inputs/', 'Day2Input.txt'),
   'utf8'
 );
-// console.log(input)
+
 const inputArray = input.split('\n');
-console.log(inputArray);
+
+// Part 1
+const partOne = (input) => {
+  //12 red, 13 green, 14 blue
+  let total = 0;
+  input.forEach((game) => {
+    let isPossible = true;
+    // split off the 'Game #: ' from the sets
+    game = game.split(': ');
+    gameNumber = game[0];
+    // split the sets string into array of sets
+    gameSets = game[1].split('; ');
+    // for each set, check if any of the number of cubes is higher than the inventory
+    gameSets.forEach((set) => {
+      set = set.split(', ');
+      set.forEach((colorGroup) => {
+        colorGroup = colorGroup.split(' ');
+        if (
+          (colorGroup[1] === 'red' && colorGroup[0] > 12) ||
+          (colorGroup[1] === 'green' && colorGroup[0] > 13) ||
+          colorGroup[0] > 14
+        )
+          isPossible = false;
+      });
+    });
+    // if none of the sets had excess cubes, it's a good set
+    if (isPossible) {
+      total += +gameNumber.split(' ')[1];
+    }
+  });
+
+  return total;
+};
+
+// console.log(partOne(inputArray));
