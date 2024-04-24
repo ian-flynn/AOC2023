0bd656c77f6147df03658b98a61b5341bded0723
Ian Flynn <ianflynnwork@gmail.com>
2024-04-24 12:15:44 -0400

    part 2

--- Day3.js
+++ Day3.js
@@ -7,18 +7,18 @@ let input = fs.readFileSync(
 );
 const inputArray = input.split('\n');
 
-// const testInput = [
-//   '467..114..',
-//   '...*......',
-//   '..35..633.',
-//   '......#...',
-//   '617*......',
-//   '.....+.588',
-//   '..592.....',
-//   '......755.',
-//   '...$.*....',
-//   '.664.598..',
-// ];
+const testInput = [
+  '467..114..',
+  '...*......',
+  '..35..633.',
+  '......#...',
+  '617*......',
+  '.....+.588',
+  '..592.....',
+  '......755.',
+  '...$.*....',
+  '.664.598..',
+];
 
 const partNumbers = (input) => {
   const results = [];
@@ -62,12 +62,39 @@ const partNumbers = (input) => {
 //   }, 0)
 // );
 
-const resultArray = partNumbers(inputArray);
-console.log(resultArray);
-console.log(
-  resultArray.reduce((acc, curr) => {
-    return Number(acc) + Number(curr);
-  }, 0)
-);
+// const resultArray = partNumbers(inputArray);
+// console.log(resultArray);
+// console.log(
+// resultArray.reduce((acc, curr) => {
+// return Number(acc) + Number(curr);
+// }, 0)
+// );
 
 // 532428
+
+const partTwo = (input) => {
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[row].length; col++) {
+      if (input[row][col] === '*') {
+        console.log(input[row]);
+
+        const top =
+          row === 0
+            ? ''
+            : input[row - 1].slice(col === 0 ? 0 : col - 1, col + 2);
+        console.log(top);
+        const sides =
+          (col === 0 ? '' : input[row][col - 1]) +
+          (col === input[row].length - 1 ? '' : input[row][col + 1]);
+        console.log(sides);
+        const bottom =
+          row === input.length - 1
+            ? ''
+            : input[row + 1].slice(col === 0 ? 0 : col - 1, col + 2);
+        console.log(row, top, sides, bottom);
+      }
+    }
+  }
+};
+
+console.log(partTwo(testInput));
