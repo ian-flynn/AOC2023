812adba4a61e0b5de1ff9bd4d5d36bd2e6c2b686
Ian Flynn <ianflynnwork@gmail.com>
2024-04-24 19:10:47 -0400

    modularized code

--- Day3.js
+++ Day3.js
@@ -65,126 +65,78 @@ const partNumberSum = (input = []) => {
     return /[^.0-9]/.test(top + right + bottom + left) ? acc + partNumber : acc;
   }, 0);
 };
-
-console.log(partNumberSum(testInput)); // 4361
-console.log(partNumberSum(inputArray)); // 532428
+// console.log(partNumberSum(testInput)); // 4361
+// console.log(partNumberSum(inputArray)); // 532428
 
 const partTwo = (input = []) => {
   const nums = numberFinder(input);
-
-  const gearMap = new Map();
-
-  const gearMapAdder = (row, col, partNumber) => {
+  const mapAdder = (map, row, col, partNumber) => {
+    console.log(partNumber, input[row][col]);
     if (input[row][col] === '*') {
-      gearMap.has(`${row}, ${col}`)
-        ? gearMap.get(`${row}, ${col}`).push(Number(partNumber))
-        : gearMap.set(`${row}, ${col}`, [Number(partNumber)]);
+      const key = `${row}, ${col}`;
+      map.set(key, (map.get(key) || []).concat(partNumber));
     }
   };
 
-  nums.forEach((el) => {
-    const {
-      partNumber,
-      row,
-      columnStart,
-      columnEnd,
-      top,
-      right,
-      bottom,
-      left,
-    } = el;
+  return Array.from(
+    nums.reduce((acc, curr) => {
+      const map2Adder = (map, row, col, partNumber, string, start, end) => {
+        console.log(partNumber, string);
+        for (let i = start; i < end; i++) {
+          // map2Adder(acc, row - 1, i, partNumber, top);
 
-    if (/[*]/.test(top)) {
-      for (let i = columnStart - 1; i <= columnEnd + 1; i++) {
-        gearMapAdder(row - 1, i, partNumber);
-      }
-    }
-    if (/[*]/.test(right)) {
-      gearMapAdder(row, columnEnd + 1, partNumber);
-    }
-    if (/[*]/.test(bottom)) {
-      for (let i = columnStart - 1; i <= columnEnd + 1; i++) {
-        gearMapAdder(row + 1, i, partNumber);
-      }
-    }
-    if (/[*]/.test(left)) {
-      gearMapAdder(row, columnStart - 1, partNumber);
-    }
-  });
-  console.log(gearMap);
+          console.log(i);
+          console.log(input[row][i]);
+          if (input[row][col] === '*') {
+            const key = `${row}, ${col}`;
+            map.set(key, (map.get(key) || []).concat(partNumber));
+          }
+        }
+      };
+      const {
+        partNumber,
+        row,
+        columnStart,
+        columnEnd,
+        top,
+        right,
+        bottom,
+        left,
+      } = curr;
 
-  let gearRatioSum = 0;
-  gearMap.forEach((partsArray) => {
-    if (partsArray.length == 2) {
-      gearRatioSum += partsArray[0] * partsArray[1];
-    }
-  });
-  return gearRatioSum;
+      if (/[*]/.test(top)) {
+        for (let i = columnStart - 1; i <= columnEnd + 1; i++) {
+          mapAdder(acc, row - 1, i, partNumber);
+        }
+        // map2Adder(acc, row - 1, null, partNumber, top);
+      }
+      if (/[*]/.test(right)) {
+        mapAdder(acc, row, columnEnd + 1, partNumber);
+        // map2Adder(
+        //   acc,
+        //   row,
+        //   columnEnd + 1,
+        //   partNumber,
+        //   right,
+        //   columnEnd + 1,
+        //   columnEnd + 2
+        // );
+      }
+      if (/[*]/.test(bottom)) {
+        for (let i = columnStart - 1; i <= columnEnd + 1; i++) {
+          mapAdder(acc, row + 1, i, partNumber);
+        }
+      }
+      if (/[*]/.test(left)) {
+        mapAdder(acc, row, columnStart - 1, partNumber);
+      }
+      return acc;
+    }, new Map())
+  ).reduce(
+    (acc, curr) => (curr[1].length === 2 ? acc + curr[1][0] * curr[1][1] : acc),
+    0
+  );
 };
 
 console.log(partTwo(testInput)); //467835
-// console.log(partTwo(inputArray)); //84051670
-
-// const partTwo = (input) => {
-//   const gearMap = new Map();
-
-//   for (let row = 0; row < input.length; row++) {
-//     for (let col = 0; col < input[0].length; col++) {
-//       if (isNaN(input[row][col])) continue;
-
-//       let start = col;
-//       let end = col;
-
-//       while (!isNaN(input[row][col])) {
-//         end = col;
-//         col++;
-//       }
-//       const partNumber = input[row].slice(start, end + 1);
-//       const top =
-//         row === 0
-//           ? ''
-//           : input[row - 1].slice(start === 0 ? 0 : start - 1, end + 2);
-//       const right = end === input[row].length - 1 ? '' : input[row][end + 1];
-//       const bottom =
-//         row === input.length - 1
-//           ? ''
-//           : input[row + 1].slice(start === 0 ? 0 : start - 1, end + 2);
-//       const left = start === 0 ? '' : input[row][start - 1];
-
-//       const gearMapAdder = (row, col) => {
-//         if (input[row][col] === '*') {
-//           gearMap.has(`${row}, ${col}`)
-//             ? gearMap.get(`${row}, ${col}`).push(Number(partNumber))
-//             : gearMap.set(`${row}, ${col}`, [Number(partNumber)]);
-//         }
-//       };
-
-//       if (/[*]/.test(top)) {
-//         for (let i = start - 1; i <= end + 1; i++) {
-//           gearMapAdder(row - 1, i);
-//         }
-//       }
-//       if (/[*]/.test(right)) {
-//         gearMapAdder(row, end + 1);
-//       }
-//       if (/[*]/.test(bottom)) {
-//         for (let i = start - 1; i <= end + 1; i++) {
-//           gearMapAdder(row + 1, i);
-//         }
-//       }
-//       if (/[*]/.test(left)) {
-//         gearMapAdder(row, start - 1);
-//       }
-//     }
-//   }
-//   let gearRatioSum = 0;
-//   gearMap.forEach((partsArray) => {
-//     if (partsArray.length == 2) {
-//       gearRatioSum += partsArray[0] * partsArray[1];
-//     }
-//   });
-//   return gearRatioSum;
-// };
-
-// console.log(partTwo(testInput)); //467835
 // console.log(partTwo(inputArray)); //84051670
